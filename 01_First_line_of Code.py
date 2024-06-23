227492446f2837d2d1ad3904e219e213f9b1a4a2
codewithniha <fa23-bai-042@isbstudent.comsats.edu.pk>
2024-06-23 13:17:36 +0500

    chapter 1

---
+++ 01_First_line_of Code.py
@@ -0,0 +1,27 @@
+print("niha you are talented")
+print("3+5")
+print(3+5)
+x="niha is a talented girl"
+print(x)
+x=(3+4)*5/7
+print(x)
+
+x="niha is talented"
+y="niha is genius"
+print(x+y)
+
+x=5
+print("niha is",x,"years old")
+print("here is the number which is printed after solution", (3+5)*86*6)
+
+x=10
+print(x)
+x=x+18
+print(x)
+
+y=3+(5*7/7+5)-10
+print(y)
+
+
+
+
